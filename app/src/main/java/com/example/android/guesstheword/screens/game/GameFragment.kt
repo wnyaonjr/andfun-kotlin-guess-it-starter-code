fa08144ad45cdfed2485e0c364bc24c67714c2c0
wnyaonjr <wnyaonjr@gmail.com>
2021-05-15 19:05:05 +0800

    - create a LiveData currentTimeString in GameViewMode - use Transformations.map to take currentTime to a String output from currentTimeString - Set timer_text to the value of currentTimeString - Delete the observer for currentTime from GameFragment

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,7 +17,6 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
-import android.text.format.DateUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -64,10 +63,6 @@ class GameFragment : Fragment() {
                 gameFinished()
                 viewModel.onGameFinishComplete()
             }
-        })
-
-        viewModel.currentTime.observe(viewLifecycleOwner, Observer { newTime ->
-            binding.timerText.text = DateUtils.formatElapsedTime(newTime)
         })
 
         return binding.root
